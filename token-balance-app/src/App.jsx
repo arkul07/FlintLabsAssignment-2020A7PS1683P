79aaef76a5e45e12fabdcda90d99f5cb8564e3e2
Arya Kulkarni <kulkarni.arya1209@gmail.com>
2024-01-08 14:45:21 +0530

    inital-commit

---
+++ token-balance-app/src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="text-3xl font-bold underline text-center">Hello</div>
+  )
+}
+
+export default App
