09026044022adf4d8a1d9ed7a24bbedaee995f30
Arya Kulkarni <kulkarni.arya1209@gmail.com>
2024-01-08 14:50:31 +0530

    added-redux-store

--- token-balance-app/src/App.jsx
+++ token-balance-app/src/App.jsx
@@ -1,12 +1,16 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+  
+  const mantleAddress = "0xDCBc586cAb42a1D193CaCD165a81E5fbd9B428d7";
+  const lineaAddress = "0xDCBc586cAb42a1D193CaCD165a81E5fbd9B428d7";
+  const kromaAddress = "0x7afb9de72A9A321fA535Bb36b7bF0c987b42b859";
 
-  return (
-    <div className="text-3xl font-bold underline text-center">Hello</div>
-  )
+
+
+  return <div className="text-3xl font-bold underline text-center">Hello</div>;
 }
 
-export default App
+export default App;
