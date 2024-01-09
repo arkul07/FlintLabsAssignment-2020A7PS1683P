5a5ec8acbaad3671fee10afd00c01c60b54c7094
Arya Kulkarni <kulkarni.arya1209@gmail.com>
2024-01-09 15:33:28 +0530

    formatting-changes

--- token-balance-app/src/App.jsx
+++ token-balance-app/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "./App.css";
 // import { Web3 } from "web3";
 import Toastify from "toastify-js";
@@ -13,45 +13,45 @@ function App() {
   const nodeEndpoint = `https://mainnet.infura.io/v3/d9687556bb264857b68824d213341b90`;
   const web3 = new Web3(new Web3.providers.HttpProvider(nodeEndpoint));
 
-
   //Store all tokens
   const [tokens, setTokens] = useState([
     {
       name: "Mantle",
       address: "0x3c3a81e81dc49A522A592e7622A7E711c06bf354",
-      contractAddress:"0xDCBc586cAb42a1D193CaCD165a81E5fbd9B428d7",
-      balance:[0]
+      contractAddress: "0xDCBc586cAb42a1D193CaCD165a81E5fbd9B428d7",
+      balance: [1],
     },
     {
       name: "Linea",
       address: "0x860B90550902248213d841d8D14F05ad489A100F",
-      contractAddress:"0xDCBc586cAb42a1D193CaCD165a81E5fbd9B428d7",
-      balance:[0]
+      contractAddress: "0xDCBc586cAb42a1D193CaCD165a81E5fbd9B428d7",
+      balance: [0],
     },
     {
       name: "Kroma",
       address: "0x91a1b0247df9B803Dcfa820522711E456909Ac80",
-      contractAddress:"0x7afb9de72A9A321fA535Bb36b7bF0c987b42b859",
-      balance:[0]
+      contractAddress: "0x7afb9de72A9A321fA535Bb36b7bF0c987b42b859",
+      balance: [0],
     },
   ]);
-
 
   //Add token
   function addToken(token) {
     setTokens((prevTokens) => [
       ...prevTokens,
-      { name: token.name, address: token.address, contractAddress:token.contractAddress },
+      {
+        name: token.name,
+        address: token.address,
+        contractAddress: token.contractAddress,
+      },
     ]);
   }
-
 
   //Store all the balances every 12 hours in an array. The previous balance will be the last element of the array
   //Compare current Balance with the previous balance to decide whether the alert should be sent or not
 
-
   //ABI
-  const epc20ABI = [
+  const erc20ABI = [
     {
       constant: true,
       inputs: [],
@@ -272,8 +272,7 @@ function App() {
       name: "Transfer",
       type: "event",
     },
-  ]; 
-
+  ];
 
   async function getTokenBalance(token) {
     try {
@@ -281,22 +280,31 @@ function App() {
       const balance = await tokenContract.methods
         .balanceOf(token.contractAddress)
         .call();
-      if(balance-balance[balance.length-1]>0.1*balance) {
-        showToastMessage() //show notification
+      console.log(balance);
+      if (balance - balance[balance.length - 1] > 0.1 * balance) {
+        showToastMessage(); //show notification
       }
-      token.balance.push(balance)
+      token.balance.push(balance);
       console.log("Token Balance:", balance);
     } catch (error) {
       console.error("Error fetching token balance:", error);
     }
   }
 
-
-  setInterval(() => {
+  function getTokensBalance() {
     tokens.map((token) => {
-      getTokenBalance(token)
-    })
-  }, 12*3600*1000); //Call every 12 hours
+      getTokenBalance(token);
+    });
+  }
+
+  useEffect(() => {
+    getTokensBalance();
+    let sec = 12 * 3600 * 1000;
+    const intervalId = setInterval(getTokensBalance, sec);
+    return () => clearInterval(intervalId);
+  }, []);
+
+  //Call every 12 hours
 
   const showToastMessage = () => {
     toast("Balance reduced by 10%!", {
@@ -311,13 +319,21 @@ function App() {
       {tokens.map((token) => {
         return (
           <div className="m-10 bg-gray-100 p-4 rounded-md shadow-md mb-4">
-            <div className="text-xl font-semibold mb-2">Token Name: {token.name}</div>
-            <div className="font-bold overflow-scroll">Token Address:{token.address}</div>
-            <div className="font-bold overflow-scroll">Contract Address: {token.contractAddress}</div>
-            <div className="font-bold">Token Balance: {token.balance[token.balance.length-1]}</div>
-            <br/>
+            <div className="text-xl font-semibold mb-2">
+              Token Name: {token.name}
+            </div>
+            <div className="font-bold overflow-scroll">
+              Token Address:{token.address}
+            </div>
+            <div className="font-bold overflow-scroll">
+              Contract Address: {token.contractAddress}
+            </div>
+            <div className="font-bold">
+              Token Balance: {token.balance[token.balance.length - 1]}
+            </div>
+            <br />
           </div>
-        )
+        );
       })}
       <ToastContainer />
     </div>
